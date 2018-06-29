78b2d3b73751482260dfbdbdd4e867f8ab3ebfb4
ramay7 <zhuynan17@gmail.com>
2018-06-29 17:04:09 +0800

    add Sarsa and SarsaLambda method

--- Experiments/Morvan/2_maze/maze_env.py
+++ Experiments/Morvan/2_maze/maze_env.py
@@ -22,6 +22,10 @@ else:
 UNIT = 40   # pixels
 MAZE_H = 6   # grid height
 MAZE_W = 6   # grid width
+RENDER_TIME_SLEEP = 0.1
+RESET_TIME_SLEEP = 0.5
+
+np.random.seed(2)
 
 
 class Maze(tk.Tk, object):
@@ -87,7 +91,7 @@ class Maze(tk.Tk, object):
 
     def reset(self):
         self.update()
-        time.sleep(1)
+        time.sleep(RESET_TIME_SLEEP)
         self.canvas.delete(self.rect)
         origin = np.array([20, 20])
         self.rect = self.canvas.create_rectangle(
@@ -99,41 +103,35 @@ class Maze(tk.Tk, object):
 
     def step(self, action):
         s = self.canvas.coords(self.rect)
-        base_action = np.array([0, 0])
-        if action == 0:   # up
-            if s[1] > UNIT:
-                base_action[1] -= UNIT
-        elif action == 1:   # down
-            if s[1] < (MAZE_H - 1) * UNIT:
-                base_action[1] += UNIT
-        elif action == 2:   # right
-            if s[0] < (MAZE_W - 1) * UNIT:
-                base_action[0] += UNIT
-        elif action == 3:   # left
-            if s[0] > UNIT:
-                base_action[0] -= UNIT
+        base_action = [0, 0]
+        if action == 0 and s[1] > UNIT:   # up
+            base_action[1] -= UNIT
+        elif action == 1 and s[1] < (MAZE_H - 1) * UNIT:   # down
+            base_action[1] += UNIT
+        elif action == 2 and s[0] < (MAZE_W - 1) * UNIT:   # right
+            base_action[0] += UNIT
+        elif action == 3 and s[0] > UNIT:   # left
+            base_action[0] -= UNIT
 
-        self.canvas.move(self.rect, base_action[0], base_action[1])  # move agent
-
+        self.canvas.move(self.rect, base_action[0], base_action[1])
         s_ = self.canvas.coords(self.rect)  # next state
 
         # reward function
         if s_ == self.canvas.coords(self.oval):
             reward = 1
-            done = True
-            s_ = 'terminal'
         elif s_ in [self.canvas.coords(self.hell1), self.canvas.coords(self.hell2), self.canvas.coords(self.hell3)]:
             reward = -1
-            done = True
-            s_ = 'terminal'
         else:
             reward = 0
-            done = False
+
+        done = True if reward != 0 else False
+        if done:
+            s_ = 'terminal'
 
         return s_, reward, done
 
     def render(self):
-        time.sleep(0.2)
+        time.sleep(RENDER_TIME_SLEEP)
         self.update()
 
 
--- Experiments/Morvan/2_maze/run.py
+++ Experiments/Morvan/2_maze/run.py
@@ -9,43 +9,73 @@ All other states:       ground      [reward = 0].
 This script is the main part which controls the update method of this example.
 The RL is in RL_brain.py.
 """
-
 from maze_env import Maze
 from RL_brain import QLearningTable
+from RL_brain import SarsaTable
+from RL_brain import SarsaLambdaTable
 
-
-def update():
+def update_Q_learning():
     for episode in range(100):
-        # initial observation
-        observation = env.reset()
-
+        observation = env.reset()   # initial observation
         while True:
-            # fresh env
-            env.render()
+            env.render()    # fresh env
+            action = RL.choose_action(str(observation))     # RL choose action based on observation
+            observation_, reward, done = env.step(action)   # RL take action and get next observation and reward
+            RL.learn(str(observation), action, reward, str(observation_))   # RL learn from this transition
+            observation = observation_      # swap observation
+            if done:    # break while loop when end of this episode
+                break
+    print('game over')  # end of game
+    env.destroy()
 
-            # RL choose action based on observation
-            action = RL.choose_action(str(observation))
 
-            # RL take action and get next observation and reward
-            observation_, reward, done = env.step(action)
-
-            # RL learn from this transition
-            RL.learn(str(observation), action, reward, str(observation_))
-
-            # swap observation
-            observation = observation_
-
-            # break while loop when end of this episode
+def update_sarsa():
+    for episode in range(100):
+        observation = env.reset()   # initial observation
+        action = RL.choose_action(str(observation))     # RL choose current action based on current observation
+        while True:
+            env.render()    # fresh env
+            observation_, reward, done = env.step(action)    # RL obtain next obervation and reward based on current action
+            action_ = RL.choose_action(str(observation_))   # RL choose next action based on next observation
+            RL.learn(str(observation), action, reward, str(observation_), action_)   # RL learn from this transition
+            observation, action = observation_, action_ # replace current observation and action with next observation and action
             if done:
                 break
-
-    # end of game
     print('game over')
     env.destroy()
 
+
+def update_sarsa_lambda():
+    for episode in range(100):
+        observation = env.reset()       # initial observation
+        action = RL.choose_action(str(observation))     # RL choose current action based on current observation
+        RL.eligibility_trace *= 0     # initialize eligibility trace
+        while True:
+            env.render()        # fresh env
+            observation_, reward, done = env.step(action)   # RL obtain next observation and reward based on current action
+            action_ = RL.choose_action(str(observation_))   # RL choose next action based on next observation
+            RL.learn(str(observation), action, reward, str(observation), action_)   # RL learn from this transition
+            observation, action = observation_, action_ # replace current observation and action with next observation and action
+            if done:
+                break
+    print('game over')
+    env.destroy()
+
+
 if __name__ == "__main__":
     env = Maze()
-    RL = QLearningTable(actions=list(range(env.n_actions)))
+
+    # Q-Learning Methon
+    # RL = QLearningTable(actions=list(range(env.n_actions)))
+    # update = update_Q_learning()
+
+    # Sarsa Method
+    RL = SarsaTable(actions = list(range(env.n_actions)))
+    update = update_sarsa()
+
+    # Sarsa-Lambda Method                     ------- This method does not work very well.
+    # RL = SarsaLambdaTable(actions = list(range(env.n_actions)))
+    # update = update_sarsa_lambda()
 
     env.after(100, update)
     env.mainloop()
